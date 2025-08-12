391cb8f90f3eb44d77ad2f3c684ec47616d7b892
Diego Andrés Candamil Puerta <diegoandres2143@gmail.com>
2025-08-12 02:57:34 -0500

    Se avanza con la función sortear amigos, aún tenemos problemas con algunos conceptos pero vamos por buen camino

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -7,10 +7,24 @@ function agregarAmigo(){
     const nombreDelAmigo = document.getElementById('amigo');
     nuevoAmigo.textContent = nombreDelAmigo.value
     if (nuevoAmigo.textContent === "") {
-        alert("No has introducido el nombre de tu amigo")
+        alert("No has introducido el nombre de tu amigo");
     } else {
     listaAmigos.appendChild(nuevoAmigo);
     nombreDelAmigo.value = "";
 }
 }
 
+function sortearAmigo(){
+    const listaAmigos = document.getElementById('listaAmigos');
+    const amigosGuardados = listaAmigos.getElementsByTagName('li');
+    
+    if (amigosGuardados.length === 0) {
+        alert("No has agregado amigos para sortear")
+    } else {
+    const amigoAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    resultado.appendChild(listaAmigos[amigoAleatorio].textContent);
+}
+}
+
+// Nota appendChild(elemento) me mueve el elemento de la lista, por ese motivo
+// no puedo utilizarlo, necesito copiar el elemento para colocarlo en la lista resultado.
