fda959dfd7c29d8123539baa211a3e1d85517b19
Diego Andrés Candamil Puerta <diegoandres2143@gmail.com>
2025-08-12 02:17:07 -0500

    Se agrega la función de Agregar Amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function agregarAmigo(){
+    const listaAmigos = document.getElementById('listaAmigos');
+    const nuevoAmigo = document.createElement('li');
+
+    const nombreDelAmigo = document.getElementById('amigo');
+    nuevoAmigo.textContent = nombreDelAmigo.value
+    if (nuevoAmigo.textContent === "") {
+        alert("No has introducido el nombre de tu amigo")
+    } else {
+    listaAmigos.appendChild(nuevoAmigo);
+    nombreDelAmigo.value = "";
+}
+}
+
